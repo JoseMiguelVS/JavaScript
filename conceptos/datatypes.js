a7bf7fdb7a87feef922f2dc9a0e2f22e688cba32
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2024-09-12 10:59:15 -0600

    01 Tipos de Datos en JavaScript

---
+++ conceptos/datatypes.js
@@ -0,0 +1,17 @@
+//Tipos de datos
+console.log(24);
+console.log(typeof 24);
+console.log(2.3);
+console.log(typeof 2.3);
+console.log(true);
+console.log(typeof true);
+console.log("Hola Mundito");
+console.log(typeof "Hola Mundito");
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log([1, 2, 3]);
+console.log(typeof[1, 2, 3]); //arreglo
+console.log({ nombre: "Joss" });
+console.log(typeof { nombre: "Joss" }); //diccionario
+
