05eda31f50147e969ec9eb0e4cf3e6297533665a
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2024-09-12 11:30:19 -0600

    03 Operadores

---
+++ conceptos/operators.js
@@ -0,0 +1,40 @@
+// operadores matematicos
+// + y -
+// * y /
+// % y ** modulo y potencia
+
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+// Operadorres string
+// concatenacion con +
+console.log("hola" + " " + "mundo");
+
+// operadores de asignacion
+let a = 1;
+a += 3;  // a = a + 3;
+a -= 3;
+a /= 3;
+a *= 3;
+a %= 3;
+a **= 3;
+console.log(a);
+
+//Operadores de comparacion
+console.log( 10 < 3);
+console.log( 10 > 3);
+console.log( 10 >= 3);
+console.log( 10 <= 3);
+console.log( 10 == 3);
+console.log( 10 === 3);
+console.log( 10 != 3);
+console.log( 10 !== 3);
+
+//Operadores logicos
+console.log(true && true); //and
+console.log(true || true);  //or
+console.log(!true); // not
