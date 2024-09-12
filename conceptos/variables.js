36752af811974616792269e425ada484750fcb96
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2024-09-12 11:15:12 -0600

    02 Variablles let y const

---
+++ conceptos/variables.js
@@ -0,0 +1,19 @@
+// Variables en JavaScript
+// Declaramos variables con let o const
+// let para variables que cambiaran de valor
+// const para variables que no cambiaran de valor
+
+let nombre;
+
+nombre = "Mikee"
+console.log(nombre);
+console.log(typeof nombre);
+
+nombre=19;
+console.log(nombre);
+console.log(typeof nombre);
+
+const pi= 3.1416;
+console.log(pi);
+console.log(typeof pi);
+// pi = 3.15;
